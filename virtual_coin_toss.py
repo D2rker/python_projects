02b8bf45aa688e0b0fcfc743edd1bb2aef77a6db
D@rker <108613154+D2rker@users.noreply.github.com>
2023-07-27 09:10:46 +0530

    Update virtual_coin_toss.py

--- virtual_coin_toss.py
+++ virtual_coin_toss.py
@@ -1,6 +1,14 @@
 import random
-random_side = random.randint(0,1)
-if random_side == 1:
-    print("head")
-else:
-    print("tails")
+
+should_end = False
+while not should_end:
+    random_side = random.randint(0,1)
+    if random_side == 1:
+        print("head")
+    else:
+        print("tails")
+
+    restart = input("type 'yes' if you want to continue:  ")
+    if restart == "no":
+        should_end = True
+        print("good bye")
